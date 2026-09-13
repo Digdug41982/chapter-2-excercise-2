b91b73433dbfe8b449abec33fb1ec484e6f1060b
andre <andre@ANDREW_LAPTOP>
2026-09-13 14:44:02 -0400

    Add project files.

---
+++ chapter 2 excercise 2.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int totalsales;
+	int ECSales;
+
+	totalsales = 8000000;
+	ECSales = (totalsales * 0.58);
+	cout << ECSales << endl;
+}
+
